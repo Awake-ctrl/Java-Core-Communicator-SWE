b6c996b13af2ae0a014dc599f068a63121440b73
Sn-G-m <112201012@smail.iitpkd.ac.in>
2025-10-24 23:40:52 +0530

    class for cosmos client operations

---
+++ module-cloud/cloud-function-app/src/main/java/cosmosoperations/CosmosOperations.java
@@ -0,0 +1,55 @@
+package cosmosoperations;
+
+import com.azure.cosmos.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import datastructures.Entity;
+import interfaces.IdbConnector;
+
+public class CosmosOperations implements IdbConnector {
+
+    CosmosClient client;
+    final String databaseName = "SE";
+    CosmosDatabase database;
+    final String containerName = "Project";
+    public void init() {
+        try {
+            client = new CosmosClientBuilder()
+                    .endpoint("https://kallepally.documents.azure.com:443/")
+                    .key("TWjalrmKnG9a6ILf4mEkalzDf8iG2OeUpaI2E36aeSkesHGRgfgg4whnjJXgFpkqIPfBHXXZymFyACDbkCcHGA==")
+                    .consistencyLevel(ConsistencyLevel.EVENTUAL)
+                    .contentResponseOnWriteEnabled(true)
+                    .buildClient();
+
+            CosmosDatabase db =  client.getDatabase(databaseName);
+
+            CosmosContainer container = db.getContainer(containerName);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public JsonNode getData(Entity request) {
+        return null;
+    }
+
+    @Override
+    public JsonNode postData(Entity request) {
+        return null;
+    }
+
+    @Override
+    public JsonNode createData(Entity request) {
+        return null;
+    }
+
+    @Override
+    public JsonNode deleteData(Entity request) {
+        return null;
+    }
+
+    @Override
+    public JsonNode updateData(Entity request) {
+        return null;
+    }
+}
