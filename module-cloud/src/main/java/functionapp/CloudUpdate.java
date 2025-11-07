e58868d35adb9a13d2401fe7759dbf8216cf06a1
Nikhil S Thomas <nikhil.s.thomas5@gmail.com>
2025-11-07 15:15:55 +0530

    Removed unnecessary verbose from 400 fix

--- module-cloud/src/main/java/functionapp/CloudUpdate.java
+++ module-cloud/src/main/java/functionapp/CloudUpdate.java
@@ -47,12 +47,15 @@ public class CloudUpdate extends CloudHelper {
             final Entity entityRequest = getObjectMapper().readValue(jsonBody, Entity.class);
 
             final IdbConnector dbConnector = DbConnectorFactory.getDbConnector("cosmo");
+            context.getLogger().info("Initialized DB Connector");
             final Response response = dbConnector.updateData(entityRequest);
+            context.getLogger().info("Received Update Response: [" + response.status_code() + "] " + response.message());
 
-            return handleResponse(response, request, context);
+            return handleResponse(response, request);
 
         } catch (Exception e) {
-            return handleError(request, context);
+            context.getLogger().info("[ERROR] Exception in CloudUpdate: " + e.getMessage());
+            return handleError(request);
         }
     }
 }
